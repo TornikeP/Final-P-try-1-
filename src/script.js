0eadacec7c4e91bc54c501532501751e7c8fdfa2
TornikeP <112972102+TornikeP@users.noreply.github.com>
2022-11-27 17:01:17 +0400

    ვერ გავასწორე

--- src/script.js
+++ src/script.js
@@ -8,22 +8,25 @@ const btn = document.querySelector('.search button')
 const main_grid_title = document.querySelector('.favorites h1')
 const main_gird = document.querySelector('.favorites .movies-grid')
 
+
+//  ესეც გაჭედვის მერე !!!!!!!!!!!!!!1
+const popup_container = document.querySelector('.popup-container')
+
         // ფილმზე ქლიქის დროს ფილმის სრულ აღწერილობაზე გადასვლა
 function add_click_effect_to_card (cards) {
     cards.forEach(card => {
         card.addEventListener('click', () => show_popup(card))
-    });
+    })
 }
 
 
-            //   ასინქრონულობა +  ჯეისონ ფაილი
-get_movie_by_search('game')
-async function get_movie_by_search (search_term) {
-    const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`)
-    const respData = await resp.json()
-    return respData.results
-    
-}
+            //    სერჩი - ასინქრონულობა +  ჯეისონ ფაილი - ეს ქვევით ჩავიტანე და by id da by trailerad გადავაკეთ - ეგეც გაჭედვის მერე
+// get_movie_by_search('game')
+// async function get_movie_by_search (search_term) {
+//     const resp = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search_term}`)
+//     const respData = await resp.json()
+//     return respData.results
+// }
 
         // სერჩში მოძებნის შედეგად  პოსტერის და აღწერილობის გამოტანა
 btn.addEventListener('click', add_searched_movies_to_dom)
@@ -33,7 +36,7 @@ btn.addEventListener('click', add_searched_movies_to_dom)
     console.log(data);
         
 main_grid_title.innerText = `Search Results...`
-main_gird.innerHTML = data.map(e => {
+main_grid.innerHTML = data.map(e => {     
     return `
             <div class="movies-grid">
                  <div class="card" data-id="${e.id}">
@@ -62,7 +65,32 @@ const cards = document.querySelectorAll('.card')
 add_click_effect_to_card(cards)
 }
 
-function show_popup (card) {
+//   popup   -  რომ გავიჭედე მაგის მერე ეს ვწერე 
+
+
+async function get_movie_by_id (id) {
+    const resp = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
+    const respData = await resp.json()
+    return respData
     
 }
+        //   trailerze ერორს მიგდებს კონსოლში ვერ წამოვაღებინე   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+async function get_movie_trailer (id) {
+    const resp = await fetch(`https://api.themoviedb.org/3/movie/${id}/video?api_key=${API_KEY}`)
+    const respData = await resp.json()
+    return respData.results[0].key 
+}
+
+function show_popup (card) {
+    popup_container.classList.add('show-popup')
+}
+
+
+    // აქამდე
+
+    // ეს გაჭედვის მერე გავაკომენტარე და ზევით რომ არის ეგ ჩავწერე . . . 
+
+// function show_popup (card) {
+//    console.log('popup is shown' + card); 
+// }
    
